80010731e3d6a96ea7ccafc17eb656be4ab5a2ff
PlyJB <ploy.j0912@gmail.com>
2025-10-18 02:22:46 +0700

    Update SpringBootTest annotation with classes parameter

--- Backend/src/test/java/com/example/apartmentmanagement/ApartmentmanagementApplicationTests.java
+++ Backend/src/test/java/com/example/apartmentmanagement/ApartmentmanagementApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.apartmentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 
-@SpringBootTest(properties = "spring.profiles.active=test")
+@SpringBootTest(
+    properties = "spring.profiles.active=test",
+    classes = ApartmentManagementApplication.class
+)
 class ApartmentmanagementApplicationTests {
 
     @Test
