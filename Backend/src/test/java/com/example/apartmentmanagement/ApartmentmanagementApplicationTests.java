e4eefc560e9e1fe84899b1ad8a86a2626b79e226
PlyJB <ploy.j0912@gmail.com>
2025-10-18 02:37:20 +0700

    Update ApartmentmanagementApplicationTests.java

--- Backend/src/test/java/com/example/apartmentmanagement/ApartmentmanagementApplicationTests.java
+++ Backend/src/test/java/com/example/apartmentmanagement/ApartmentmanagementApplicationTests.java
@@ -2,15 +2,12 @@ package com.example.apartmentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 
-@SpringBootTest(
-    properties = "spring.profiles.active=test",
-    classes = ApartmentManagementApplication.class
-)
+@SpringBootTest(properties = "spring.profiles.active=test")
 class ApartmentmanagementApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
 }
