81bdd1bebce81f8310e6ba00fd4d27f88579fc86
PlyJB <ploy.j0912@gmail.com>
2025-10-18 02:55:06 +0700

    Remove exclusion of SystemMetricsAutoConfiguration

--- Backend/src/main/java/com/example/apartmentmanagement/ApartmentManagementApplication.java
+++ Backend/src/main/java/com/example/apartmentmanagement/ApartmentManagementApplication.java
@@ -2,9 +2,8 @@ package com.example.apartmentmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 
-@SpringBootApplication(exclude = {SystemMetricsAutoConfiguration.class})
+@SpringBootApplication
 public class ApartmentManagementApplication {
 
     public static void main(String[] args) {
