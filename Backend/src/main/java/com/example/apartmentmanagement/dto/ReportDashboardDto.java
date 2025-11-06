31ccb850c2f58c9f9984f2a7a4e4e78c2571c7a0
Ploy Jomboon <ploy.j0912@gmail.com>
2025-11-06 14:37:18 +0700

    fixed maintain count for rooms

--- Backend/src/main/java/com/example/apartmentmanagement/dto/ReportDashboardDto.java
+++ Backend/src/main/java/com/example/apartmentmanagement/dto/ReportDashboardDto.java
@@ -32,6 +32,6 @@ public class ReportDashboardDto {
 
         private BigDecimal waterUsage;
         private BigDecimal electricityUsage;
-        private Double maintenanceCost;
+        private long maintenanceCount;
     }
 }
